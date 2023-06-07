79ff3b58e4859f847835df2f352c5df9bc010805
Dhruv Rekhawat <dhruvrekhawat5@gmail.com>
2023-06-08 02:12:44 +0530

    made navbar sticky

--- src/pages/blogs/Blogs.jsx
+++ src/pages/blogs/Blogs.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './blog.css'
 const Blogs = () => {
   return (
-    <div className='section__padding section__margin'>Blogs</div>
+    <div className='section__padding section__margin'>blogs</div>
   )
 }
 
